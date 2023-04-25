e109ec17689e5b4c0b1e6e832eeef4ca997afbac
anji0114 <anji.tnk.140114@gmail.com>
2023-04-25 17:52:38 +0900

    styleの調整

--- src/components/Dashboard/DashboardHeading.tsx
+++ src/components/Dashboard/DashboardHeading.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const DashboardHeading: FC<Props> = ({ title, icon, children }) => {
   return (
-    <div className="flex items-center justify-between py-[14px] px-5 border border-[#d0d7de] rounded-md bg-white">
+    <div className="flex items-center justify-between py-[14px] px-5 border border-[#d0d7de] rounded-md bg-white min-h-[72px]">
       <h1 className="flex items-center gap-2.5">
         {icon}
         <span className="inline-block whitespace-nowrap font-medium">
